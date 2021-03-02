464a515e07705fd0eeae547f418adbf736067839
lionad <tangnad@qq.com>
2021-03-02 11:27:41 +0800

    fix: last

--- app/src/main.js
+++ app/src/main.js
@@ -1,13 +1,35 @@
 import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { mapActions } from 'vuex'
+import Element from 'element-ui'
+// import Fragment from 'vue-fragment' // todo
+import 'reset-css'
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false
+import Editor from '@/views/editor'
+import DefaultModule from '@/modules'
+import store from '@/store/editor'
+import '@/views/editor/styles/index.less'
 
-new Vue({
-  router,
+Vue.use(Element)
+// Vue.use(Fragment) // todo
+
+const LegoEditor = new Vue({
+  name: 'lego-editor',
   store,
-  render: h => h(App),
+  methods: {
+    ...mapActions('editor', ['INSTALL_MODULES']),
+  },
+  mounted() {
+    this.INSTALL_MODULES(DefaultModule.moduleList)
+  },
+  render() {
+    return <Editor />
+  },
 }).$mount('#app')
+
+// 导出模块，方面引入或做成多入口
+LegoEditor.install = function(Vue) {
+  Vue.component(LegoEditor.name, LegoEditor)
+}
+
+export default LegoEditor
