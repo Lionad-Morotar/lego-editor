9eaaf73bf015c02a4aacea3f36dfcc99346cf51a
lionad <tangnad@qq.com>
2021-03-09 23:08:14 +0800

    text: “组件” -> "模块"

--- app/src/modules/index.js
+++ app/src/modules/index.js
@@ -16,32 +16,32 @@ export default {
     const moduleList = [
       {
         title: '基础文本',
-        description: '非常简单的文本组件',
+        description: '非常简单的文本模块',
         name: BaseText.name,
         component: BaseText,
         cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/pic.png',
-        categories: [baseType, '文字组件']
+        categories: [baseType, '文字模块']
       },
       {
         title: '基础图片',
-        description: '非常简单的图片组件',
+        description: '非常简单的图片模块',
         name: BaseImage.name,
         component: BaseImage,
         cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/pic.png',
-        categories: [baseType, '图片组件']
+        categories: [baseType, '图片模块']
       },
       {
-        title: '对比组件',
-        description: '简单的对比组件',
+        title: '对比模块',
+        description: '简单的对比模块',
         name: Compare.name,
         component: Compare,
         cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/pic.png',
-        categories: ['对比组件']
+        categories: ['对比模块']
       }
     ]
     const exampleModuleList = [
       {
-        title: '测试组件',
+        title: '测试模块',
         description: '供开发时参考及落文档用',
         name: Example.name,
         component: Example,
