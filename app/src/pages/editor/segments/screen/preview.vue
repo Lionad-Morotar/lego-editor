632e2db2edb1e0d64075c19b2582d9aa0edeb74d
lionad <tangnad@qq.com>
2021-03-10 11:53:13 +0800

    styles: 预览界面样式

--- app/src/pages/editor/segments/screen/preview.vue
+++ app/src/pages/editor/segments/screen/preview.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="preview">
-    <template v-for="m in modules">
-      <instance :module="m" :key="m.uuid" />
-    </template>
+  <div class="mask">
+    <div class="preview">
+      <template v-for="m in modules">
+        <instance :module="m" :key="m.uuid" />
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import TestData from '@/modules/test/example-data'
+import { mapState, mapActions } from 'vuex'
 export default {
   components: {
     Instance: {
@@ -22,21 +23,27 @@ export default {
     }
   },
   computed: {
+    ...mapState('editor', {
+      installedModules: state => state.modules
+    }),
     ...mapState('screen', {
       modules: state => state.modules
     })
   },
-  created () {
-    this.modules.length = 0
-    const findInits = TestData.map(x => x)
-    // const toDisplayModule =
-    console.log('findInits: ', findInits)
+  methods: {
+    ...mapActions('editor', [
+      'INSTALL_MODULES',
+      'CLEAR_MODULE'
+    ]),
+    ...mapActions('screen', [
+      'ADD_MODULE'
+    ])
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.preview {
+.mask {
   position: fixed;
   left: 0;
   right: 0;
@@ -44,7 +51,8 @@ export default {
   bottom: 0;
   margin-top: 55px;
   padding: 0 calc(50vw - 188px);
-  padding-top: 20px;
+  padding-top: 45px;
+  padding-bottom: calc(55px + 45px);
   width: 100vw;
   height: 100vh;
   background: white;
@@ -55,5 +63,11 @@ export default {
   &::-webkit-scrollbar-track {
     width: 0;
   }
+}
+.preview {
+  margin: 0 auto;
+  width: 375px;
+  min-height: 655px;
+  box-shadow: 0 0 7px 1px #ddd;
 }
 </style>
