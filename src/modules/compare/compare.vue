1c3e2d07ab8606e4116bf255947b51dc50135229
lionad <tangnad@qq.com>
2021-03-19 00:47:32 +0800

    styles: 调整样式使高亮选框包裹住子项的标题

--- src/modules/compare/compare.vue
+++ src/modules/compare/compare.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="vs">
-    <div class="left">
-      <div class="title">{{ compare.left.title }}</div>
-      <img
-        :src="compare.left.url.url"
-        :style="styles.leftImage"
-      />
-      <div class="description" :style="styles.leftDes">
-        {{ compare.left.description.text }}
+  <div class="compare-wrapper">
+    <div class="compare">
+      <div class="left">
+        <div class="title">{{ compare.left.title }}</div>
+        <img
+          :src="compare.left.url.url"
+          :style="styles.leftImage"
+        />
+        <div class="description" :style="styles.leftDes">
+          {{ compare.left.description.text }}
+        </div>
       </div>
-    </div>
-    <div class="right">
-      <div class="title">{{ compare.right.title }}</div>
-      <img
-        :src="compare.right.url.url"
-        :style="styles.rightImage"
-      />
-      <div class="description" :style="styles.rightDes">
-        {{ compare.right.description.text }}
+      <div class="right">
+        <div class="title">{{ compare.right.title }}</div>
+        <img
+          :src="compare.right.url.url"
+          :style="styles.rightImage"
+        />
+        <div class="description" :style="styles.rightDes">
+          {{ compare.right.description.text }}
+        </div>
       </div>
     </div>
   </div>
@@ -48,3 +50,132 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.compare-wrapper {
+  margin-top: 13px;
+  padding-top: 12px;
+}
+.compare {
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+  position: relative;
+  margin: auto;
+  padding: 20px 12px 10px 12px;
+  border: solid 1px #4381ff;
+  border-radius: 5px;
+  min-height: 155px;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: -20px;
+    left: calc(50% - 17.5px);
+    width: 35.5px;
+    height: 35.5px;
+    border: solid 1px #4381ff;
+    background: white;
+    transform: rotate(45deg);
+    clip-path: polygon(0% 100%, 100% 100%, 100% 0%);
+  }
+  &:after {
+    content: 'VS';
+    position: absolute;
+    top: -0.5em;
+    left: 50%;
+    width: 100px;
+    font-size: 18px;
+    font-weight: bold;
+    text-align: center;
+    letter-spacing: 1px;
+    text-indent: 0.3em;
+    transform: translateX(-50%);
+  }
+
+  .left,
+  .right {
+    flex-shrink: 0;
+    width: 140px;
+  }
+  .title {
+    word-break: break-all;
+  }
+
+  .left {
+    .title {
+      left: -1px;
+      background: #5200d1;
+      border-bottom-left-radius: 0;
+      &:after,
+      &:before {
+        left: 0;
+      }
+      &:after {
+        border-top-left-radius: 100%;
+        clip-path: polygon(0% 0%, 100% 0%, 0 100%);
+      }
+      &:before {
+        clip-path: polygon(0% 0%, 90% 0%, 0 90%);
+        background: #5200d1;
+      }
+    }
+  }
+  .right {
+    .title {
+      right: -1px;
+      background: #ac83ec;
+      border-bottom-right-radius: 0;
+      &:after,
+      &:before {
+        right: 0;
+      }
+      &:after {
+        border-top-right-radius: 100%;
+        clip-path: polygon(0% 0%, 100% 0%, 100% 100%);
+      }
+      &:before {
+        clip-path: polygon(10% 0%, 100% 0%, 100% 90%);
+        background: #ac83ec;
+      }
+    }
+  }
+
+  .title {
+    position: absolute;
+    top: -12px;
+    width: 75px;
+    height: 24px;
+    line-height: 24px;
+    text-align: center;
+    border-radius: 4px;
+    letter-spacing: 0.3px;
+    font-size: 13px;
+    font-weight: 300;
+    color: white;
+
+    &:after,
+    &:before {
+      content: '';
+      position: absolute;
+      bottom: -12px;
+      width: 12px;
+      height: 12px;
+    }
+    &:after {
+      background: white;
+    }
+  }
+  img {
+    width: 100%;
+    height: 84px;
+    background: #fefefe;
+  }
+  .description {
+    min-height: 2em;
+    font-size: 12px;
+    color: #333;
+    line-height: 1.5em;
+  }
+}
+</style>
