0b8c67ea4dc49f2f1e390fa3b2be3407ba7dc006
lionad <tangnad@qq.com>
2021-03-21 14:36:32 +0800

    restructure

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,12 @@
 import clone from 'lodash.clonedeep'
 
 import { ModuleTypeEnums } from '@/constant'
+/* 基础组件 */
 import Example from './example'
-import BaseText from './base-text'
-import BaseTextarea from './base-textarea'
-import BaseImage from './base-image'
+import Text from './text'
+import Textarea from './textarea'
+import Image from './image'
+/* 高级组件 */
 import Compare from './compare'
 import Procedure from './procedure'
 
@@ -19,24 +21,24 @@ export default {
       {
         title: '基础文本',
         description: '文本组件，可以调节字体大小，颜色等样式',
-        name: BaseText.name,
-        component: BaseText,
+        name: Text.name,
+        component: Text,
         cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/lego/preview-basic-text.png',
         categories: [baseType, '文字模块']
       },
       {
         title: '文本区域',
         description: '文本区域，可看作可以换行的文本组件',
-        name: BaseTextarea.name,
-        component: BaseTextarea,
+        name: Textarea.name,
+        component: Textarea,
         cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/lego/preview-basic-textarea.png',
         categories: [baseType, '文字模块']
       },
       {
         title: '基础图片',
         description: '基础图片测试',
-        name: BaseImage.name,
-        component: BaseImage,
+        name: Image.name,
+        component: Image,
         cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/pic.png',
         categories: [baseType, '图片模块']
       },
