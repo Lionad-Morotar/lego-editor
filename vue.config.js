a1a028ff605a8bc397e14ef25010eb02ceeb8a14
Lionad <tangnad@qq.com>
2021-03-29 15:30:39 +0800

    lint & todo

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
       template: 'public/index.html',
       filename: 'index.html',
       title: 'index'
-    },
+    }
   },
   chainWebpack: config => {
     config.resolve.alias
