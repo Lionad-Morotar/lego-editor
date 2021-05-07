4bddb21a0dea4b973d15ecc9eba300ea5a7346af
lionad <tangnad@qq.com>
2021-05-07 21:59:36 +0800

    feat: 模块分类

--- src/modules/index.js
+++ src/modules/index.js
@@ -5,8 +5,8 @@ import { ModuleTypeEnums } from '@/constant'
 /* 基础模块 */
 import Slider from './slider'
 import Space from './space'
-import String from './text'
-import Stringarea from './textarea'
+import Header from './header'
+import StringArea from './textarea'
 import Image from './image'
 import Button from './button'
 /* 高级模块 */
@@ -19,31 +19,37 @@ import Procedure from './procedure'
 
 const baseType = ModuleTypeEnums.BASE
 
-const moduleList = [
-  /* 基础模块 */
+/* 文本模块 */
+
+const textModules = [
+  {
+    title: '标题文本',
+    description: '标题文本',
+    component: Header,
+    name: Header.name,
+    cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/lego/preview-basic-text.png',
+    categories: [baseType, '文本模块']
+  },
+  {
+    title: '基础文本',
+    description: '基础文本区域',
+    component: StringArea,
+    name: StringArea.name,
+    cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/lego/preview-basic-textarea.png',
+    categories: [baseType, '文本模块']
+  }
+]
+
+/* 图片模块 */
+
+const imageModules = [
   {
     title: '轮播模块',
     description: '轮播图',
     component: Slider,
     name: Slider.name,
     cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/lego/preview-slider.png',
-    categories: [baseType, '基础布局']
-  },
-  {
-    title: '单行文本',
-    description: '基础单行文本模块，可以调节字体大小，颜色等样式',
-    component: String,
-    name: String.name,
-    cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/lego/preview-basic-text.png',
-    categories: [baseType, '基础布局']
-  },
-  {
-    title: '文本区域',
-    description: '基础文本区域',
-    component: Stringarea,
-    name: Stringarea.name,
-    cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/lego/preview-basic-textarea.png',
-    categories: [baseType, '基础布局']
+    categories: [baseType, '图片模块']
   },
   {
     title: '图片模块',
@@ -51,15 +57,21 @@ const moduleList = [
     component: Image,
     name: Image.name,
     cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/lego/preview-image.png',
-    categories: [baseType, '基础布局']
-  },
+    categories: [baseType, '图片模块']
+  }
+]
+
+const moduleList = [
+  /* 基础模块 */
+  ...textModules,
+  ...imageModules,
   {
     title: '按钮模块',
     description: '基础按钮模块',
     component: Button,
     name: Button.name,
     cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/lego/preview-basic-button.png',
-    categories: [baseType, '基础布局']
+    categories: [baseType, '按钮模块']
   },
   {
     title: '空白占位',
@@ -67,7 +79,7 @@ const moduleList = [
     component: Space,
     name: Space.name,
     cover: 'https://baxing-lionad.oss-cn-shanghai.aliyuncs.com/lego/preview-space.png',
-    categories: [baseType, '基础布局']
+    categories: [baseType, '分隔模块']
   },
   /* 高级模块 */
   {
